162e357a3e0edf345cb9fe5b069c984526cf36a9
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-01 01:32:26 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,26 @@
-# install flask from  pip3
-
-package { 'python3-pip':
+# Ensure Python 3.8 is installed
+package { 'python3.8':
   ensure   => 'installed',
+  provider => 'apt',
 }
 
-package { 'flask':
-  ensure   => '2.1.0',
+# Ensure pip is installed for Python 3
+package { 'python3-pip':
+  ensure   => 'installed',
+  provider => 'apt',
+  require  => Package['python3.8'],  # Ensure pip is installed after Python 3.8
+}
+
+# Ensure Flask is installed using pip3
+package { 'Flask':
+  ensure   => '2.1.0',  # Correcting the version to 2.1.0 as per your requirements
   provider => 'pip3',
-  require  => Package['python3-pip'],
+  require  => Package['python3-pip'],  # Ensure Flask is installed after pip
+}
+
+# Ensure Werkzeug is installed using pip3
+package { 'Werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['Flask'],  # Ensure Werkzeug is installed after Flask
 }
