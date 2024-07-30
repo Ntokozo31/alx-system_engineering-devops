bcb06c5cf925beafa3a665fce1b635094bb55f92
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-30 08:07:34 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,6 +8,7 @@ import sys
 
 REST_API = "https://jsonplaceholder.typicode.com"
 
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python script.py <userId>")
@@ -18,21 +19,34 @@ def main():
         return
     user_id = int(user_id)
 
+    # Fech user details
     users_url = "{}/users/{}".format(REST_API, user_id)
     user_response = requests.get(users_url)
     user_data = user_response.json()
 
+    # Fetch todos
     todos_url = "{}/todos".format(REST_API)
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
 
+    # Extract employee name
     employee_name = user_data.get('name')
+
+    # Filter task for the user
     user_tasks = [todo for todo in todos if todo['userId'] == user_id]
     completed_tasks = [task for task in user_tasks if task['completed']]
 
-    print("Employee {} is done with tasks ({}/{}):" .format(employee_name, len(completed_tasks), len(user_tasks)))
+    # Output the results
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name,
+            len(completed_tasks),
+            len(user_tasks)
+        )
+    )
     for task in completed_tasks:
         print("\t {}".format(task['title']))
+
 
 if __name__ == '__main__':
     main()
