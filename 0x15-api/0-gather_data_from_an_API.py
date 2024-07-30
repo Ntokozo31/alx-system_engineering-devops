04962031869dcff237799c7a61dbe56405259386
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-30 05:54:52 -0700

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+'''
+Request employee data from API
+'''
+import requests
+import sys
+
+REST_API = "https://jsonplaceholder.typicode.com"
+
+def main():
+    if len(sys.argv[1]) != 2:
+        print("Usage: python script.py <userId>")
+        return
+    user_Id = sys.argv[1]
+    if not userId.isdigit():
+        print("Error: The user ID must be an integer")
+        return
+    user_Id = int(userId)
+
+    user_url = f"{REST_API}/user/{user_Id}"
+    user_response = requests.get(user_url)
+    user_data = user_response.json()
+
+    todos_url = f"{REST API}/todos"
+    todos_response = request.get(todos_url)
+    todos = todos_response.json()
+
+    employee_name = user_data.get('name')
+    user_tasks = [todo for todo in todos if todo['userId'] == user_id]
+    completed_tasks = [task for task in user_task if task['completed']]
+
+    print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(user_tasks)}):")
+    for task in completed_tasks:
+        print(f"\t {tasks[tittle]}")
