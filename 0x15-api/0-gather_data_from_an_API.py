c7e37ddfc694bd7efa6362fb8ade5796d9f17927
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-30 06:35:03 -0700

    0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,14 +12,14 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python script.py <userId>")
         return
-    user_Id = sys.argv[1]
-    if not user_Id.isdigit():
+    user_id = sys.argv[1]
+    if not user_id.isdigit():
         print("Error: The user ID must be an integer")
         return
-    user_Id = int(user_Id)
+    user_id = int(user_id)
 
-    users_url = f"{REST_API}/user/{user_Id}"
-    user_response = requests.get(user_url)
+    users_url = f"{REST_API}/users/{user_id}"
+    user_response = requests.get(users_url)
     user_data = user_response.json()
 
     todos_url = f"{REST_API}/todos"
@@ -27,12 +27,12 @@ def main():
     todos = todos_response.json()
 
     employee_name = user_data.get('name')
-    user_tasks = [todo for todo in todos if todo['userId'] == user_id]
+    user_task = [todo for todo in todos if todo['userId'] == user_id]
     completed_tasks = [task for task in user_task if task['completed']]
 
     print(f"Employee {employee_name} is done with tasks({len(completed_tasks)}/{len(user_tasks)}):")
     for task in completed_tasks:
-        print(f"\t {task[tittle]}")
+        print(f"\t {task[title]}")
 
 if __name__ == '__main__':
     main()
