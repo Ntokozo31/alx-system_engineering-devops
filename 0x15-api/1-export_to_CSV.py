6dd17e5fe40c5b4adb8c26bab089f2f6d586a5db
Ntokozo31 <dubentokozo45@yahoo.com>
2024-07-30 12:07:05 -0700

    1-export_to_CSV.py

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,14 +9,14 @@ import sys
 
 def fetch_user_data(user_id):
     """Fetch user data from API"""
-    url_user = 'https://jsonplaceholder.typicode.com/user/{}'.format(user_id)
+    url_user = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     response = requests.get(url_user)
     return response.json()
 
 
 def fetch_tasks(user_id):
     """Fetch task data from API"""
-    base_url = 'https://jsonplaceholder.typicode.com/user/{}/todos'
+    base_url = 'https://jsonplaceholder.typicode.com/users/{}/todos'
     url_tasks = base_url.format(user_id)
     response = requests.get(url_tasks)
     return response.json()
@@ -24,12 +24,12 @@ def fetch_tasks(user_id):
 
 def write_to_csv(user_id, username, tasks):
     """Write the task data to csv file"""
-    filename = '{}'.csv.format(user_id)
+    filename = '{}.csv'.format(user_id)
     with open(filename, mode='w', newline='') as file:
-        write = csv.write(file, qouting=csv.QUATE_ALL)
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
         for task in tasks:
-            raw = [user_id, username, task['completed'], task['title']]
-            writer.writeraw(raw)
+            row = [user_id, username, task['completed'], task['title']]
+            writer.writerow(row)
 
 
 def main():
@@ -38,10 +38,10 @@ def main():
         return
     user_id = sys.argv[1]
     user_data = fetch_user_data(user_id)
-    tasks = fatch_tasks(user_id)
+    tasks = fetch_tasks(user_id)
 
     username = user_data.get('username')
-    write_to_csv = (user_id, username, tasks)
+    write_to_csv(user_id, username, tasks)
 
 
 if __name__ == '__main__':
