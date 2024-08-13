646705cba4eec3f2e65fd731ca3b6a015f26631c
Ntokozo31 <dubentokozo45@yahoo.com>
2024-08-13 12:23:10 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,12 +12,12 @@ from sys import argv
 def top_ten(subreddit):
     """Return the top 10 posts"""
     user = {'User-Agent': 'Ntokozo'}
-    url = request.get{'https://www.reddit.com/r/{}/hot/.json?limit=10'
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
                       .format(subreddit), headers = user).json()
     try:
         for post in url.get('data').get('children'):
             print(post.get('data').get('title'))
-    expcept Exception:
+    except Exception:
         print(None)
 
 
