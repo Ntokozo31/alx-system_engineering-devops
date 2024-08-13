01cc3c48ae7a7cddd27927a83267af0179f2beaa
Ntokozo31 <dubentokozo45@yahoo.com>
2024-08-13 12:12:51 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,12 @@ and return the number of subscribers
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subredit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozille/5.0"}
-    response = request.get(url, headers=headers, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
         subscribers = data['data']['subscribers']
