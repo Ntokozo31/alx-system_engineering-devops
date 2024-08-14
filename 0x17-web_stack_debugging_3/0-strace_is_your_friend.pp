fbacae6ecbfc9532d532c5aaf94a29a2336c8b99
Ntokozo31 <dubentokozo45@yahoo.com>
2024-08-14 19:00:53 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # We are fixing `phpp` extension to `php` in the WordPress file
 
 exec { 'fix-wordpress':
-  command => 'sed -i/ s/phpp/php/g /var/www/html/wp-settings.php',
-  path    => '/usr/local/bin/:/bin/'
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/bin/', '/bin/'],
 }
